256d75fcfcd1f2a6963461b06fd3b8a0007d61ce
M.E. Bassett <meb@triviumre.com>
2017-11-13 16:10:17 +0000

    oops, this needs the Elm types from FileReader... 	modified:   src/Elm/Record.hs

--- src/Elm/Record.hs
+++ src/Elm/Record.hs
@@ -95,7 +95,7 @@ instance HasTypeRef ElmPrimitive where
   renderRef EString = pure "String"
   renderRef EUnit = pure "()"
   renderRef EFloat = pure "Float"
-  renderRef ENativeFile = pure "Servant.Multipart.MultipartData"
+  renderRef ENativeFile = pure "List FileReader.NativeFile"
 
 -- | Puts parentheses around the doc of an elm ref if it contains spaces.
 elmRefParens :: ElmPrimitive -> Doc -> Doc
